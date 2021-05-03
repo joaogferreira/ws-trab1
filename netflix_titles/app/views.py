fcba56177750e42749bb070191bb919f46cf26c3
João Ferreira <jgmpof@gmail.com>
2021-05-03 19:08:46 +0100

    movies & tv shows & search pages

--- netflix_titles/app/views.py
+++ netflix_titles/app/views.py
@@ -9,6 +9,7 @@ import requests
 
 from app.repository import Repository
 
+
 repo_name = "netflix"
 endpoint = "http://localhost:7200"
 repository = Repository(repo_name, endpoint)
@@ -28,4 +29,44 @@ def home(request):
         }
 
     return render(request, 'home.html', tparams)
+
+def movies(request):
+    assert isinstance(request, HttpRequest)
+
+    tparams = {
+        'movies' : [
+            {'title': 'O Padrinho', 'director': 'Director X', 'release_year': 'Year Y', 'listed_in': 'comedy'},
+            {'title': 'O Padrinho II', 'director': 'Director A', 'release_year': 'Year Z', 'listed_in': 'drama'},
+            {'title': 'O Padrinho III', 'director': 'Director B', 'release_year': 'Year W', 'listed_in': 'comedy and drama'},
+            {'title': 'O Grande Chefão', 'director': 'Director C', 'release_year': 'Year H', 'listed_in': 'war'},
+        ],
+        'base' : 'base.html'
+    }
+
+    return render(request, 'movies.html', tparams)
+
+
+def tvshows(request):
+    assert isinstance(request, HttpRequest)
+
+    tparams = {
+        'movies': [
+            {'title': 'Serie I', 'director': 'Director Quim Barreiros', 'release_year': '2021', 'listed_in': 'comedy'},
+            {'title': 'Serie II', 'director': 'Director Yashin', 'release_year': '2011', 'listed_in': 'drama'},
+            {'title': 'Serie III', 'director': 'Director Iker Casillas', 'release_year': '2010', 'listed_in': 'comedy and drama'},
+            {'title': 'Serie IV', 'director': 'Director C', 'release_year': 'Year H', 'listed_in': 'war'},
+        ],
+        'base': 'base.html'
+    }
+
+    return render(request, 'tvshows.html', tparams)
+
+def search(request):
+    assert isinstance(request, HttpRequest)
+
+    tparams = {
+        'base': 'base.html'
+    }
+
+    return render(request, 'search.html', tparams)
 
--- netflix_titles/netflix_titles/urls.py
+++ netflix_titles/netflix_titles/urls.py
@@ -20,4 +20,7 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('movies/', views.movies, name='movies'),
+    path('tvshows/', views.tvshows, name='tvshows'),
+    path('search/', views.search, name='search')
 ]
