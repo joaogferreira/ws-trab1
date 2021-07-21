989b3bc8690cf3f68c216bd2582f0c28a0e30e62
joaogferreira <47529390+joaogferreira@users.noreply.github.com>
2021-07-21 19:15:03 +0100

    Update settings.py

--- netflix_titles/netflix_titles/settings.py
+++ netflix_titles/netflix_titles/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8^_rv04!ufyp#4)$9-4l!^x7wslpi&m#$_7a(g-3*zkooo^by'
+# missing
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
